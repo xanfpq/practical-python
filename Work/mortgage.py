c48c71237192648f977225d7b577b0b8847e3bc2
xanfpq <xanfpq@gmail.com>
2022-03-23 23:38:17 +0100

    Finish 1 Introduction to Python

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -30,7 +30,7 @@ while principal > 0:
         total_payment = principal * (1 + rate / 12)
     principal = principal * (1 + rate / 12) - total_payment
     total_paid = total_paid + total_payment
-    print(months, round(total_paid, 2), round(principal, 2))
+    print(f'{months:3d} {total_paid:10.2f} {principal:10.2f}')
 
-print('Total paid', round(total_paid, 2))
-print('Months', months)
+print(f'Total paid {total_paid:0.2f}')
+print(f'Months: {months}')
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,45 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
+from fileinput import filename
+import sys
+
+def portfolio_cost(filename):
+    total_cost = 0
+    with open(filename, 'rt') as f:
+        header = f.readline().split(',')
+        row = 0
+        for line in f:
+            row += 1
+            data = line.split(',')
+            try:
+                total_cost += int(data[1]) * float(data[2])
+            except ValueError as e:
+                print(f'Row {row}: {e}')
+                continue
+    return total_cost
+
+def portfolio_cost_csv(filename):
+    total_cost = 0
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        header = next(rows)
+        line = 0
+        for row in rows:
+            line += 1
+            try:
+                total_cost += int(row[1]) * float(row[2])
+            except ValueError as e:
+                print(f'Row {line}: {e}')
+                continue
+    return total_cost
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = './Work/Data/portfolio.csv'
+
+#cost = portfolio_cost('./Work/Data/missing.csv')
+cost = portfolio_cost_csv(filename)
+print(f'Total cost {cost}')
